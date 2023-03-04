b603346d006922c926ef477be6fd024c1e181b78
MoonGoon <moongoon.cnu@gmail.com>
2023-03-04 11:18:15 +0900

    Initial Commit

---
+++ SimpleTouch/SimpleTouchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleTouchApp.swift
+//  SimpleTouch
+//
+//  Created by 문영균 on 2023/03/04.
+//
+
+import SwiftUI
+
+@main
+struct SimpleTouchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
